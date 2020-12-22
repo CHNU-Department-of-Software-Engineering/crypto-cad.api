31ff3d07082fb8c00162ceb5930887429f89f7a1
Mykola Kushniryk <36865549+mkushniryk@users.noreply.github.com>
2020-12-22 12:38:34 +0200

    Delete frontend code

--- src/frontend/src/constants/method.ts
+++
@@ -1,8 +0,0 @@
-export const TYPE_CIPHER = 'cipher'
-export const TYPE_HASH = 'hash'
-
-export const OPERATION_ENCRYPTION = 'encryption'
-export const OPERATION_DECRYPTION = 'decryption'
-
-export const RELATION_PARENT = 'parent'
-export const RELATION_CHILD = 'child'
